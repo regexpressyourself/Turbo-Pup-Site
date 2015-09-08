4cdd868aa232a66356d4b1a0acb9e20e971f5e1c
Sam Messina <samuel.messina@gmail.com>
2015-09-08 05:56:14 -0500

    added a question popup to login system

--- login.php
+++ login.php
@@ -65,11 +65,15 @@ if (isset($_POST['submit'])) {
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
 
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -86,23 +90,50 @@ if (isset($_POST['submit'])) {
 <form name="vaultLogin" id="vaultLogin" action="login.php" method="post" >
     <div class="row">
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username *" id="username" name="username" required data-validation-required-message="Please enter a username.">
+                <input type="text" class="form-control" placeholder="Name Tag *" id="username" name="username" required data-validation-required-message="Please enter your name tag.">
                 <p class="help-block text-danger"></p>
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" placeholder="Combination *" id="password" name="password" required data-validation-required-message="Please enter a password.">
+                <input type="password" class="form-control" placeholder="Combination *" id="password" name="password" required data-validation-required-message="Please enter your combination.">
                 <p class="help-block text-danger"></p>
             </div>
         <div class="clearfix"></div>
             <div id="success"></div>
         <div class="text-center">
             <button name="submit" type="submit" value="Submit" form="vaultLogin" class="btn btn-xl">Open Vault</button>
-        </div>
-    </div>
-</form>
 
+        </div>
+
+    </div>
+  </form>
+
+  <div class="text-center">
+    <a href="#question-modal" data-toggle="modal"><i class="glyphicon glyphicon-question-sign"></i></a>
+  </div>
 
 </div>
+
+<!-- Modal -->
+<div id="question-modal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">So what's the vault?</h4>
+      </div>
+      <div class="modal-body">
+        <p>The vault is where we hide the really good stuff. You can get your own name tag and combination at any of our live shows.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
